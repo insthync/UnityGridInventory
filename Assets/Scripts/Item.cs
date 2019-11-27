63d2bf1fe79e5498c75d56c84edde9ac27e6be98
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-11-27 23:31:07 +0700

    Demo item

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "Item", menuName = "Item", order = -999)]
 public class Item : ScriptableObject
 {
+    [Min(1)]
     public int sizeX;
+    [Min(1)]
     public int sizeY;
+    public Color color;
 }
